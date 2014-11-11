a7eb6a5907af8d2fbea8b301ace15c696156591a
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2014-11-11 09:34:27 -0500

    To make use of the latest JDK 1.8

--- gfxd-server/startserver.sh
+++ gfxd-server/startserver.sh
@@ -7,7 +7,6 @@ if [ -z "$LOCATOR_1_PORT_10334_TCP_ADDR" ]; then
 	else
 	  gfxd server start -log-level=config -client-bind-address=0.0.0.0 -J-Dgemfirexd.prefer-netserver-ipaddress=true -client-port=$CLIENT_PORT -dir=/opt/gfxd/server1
 	fi
-
 else
 # Assume a linked container called Locator is available	
   gfxd server start -log-level=config -client-bind-address=0.0.0.0 -J-Dgemfirexd.prefer-netserver-ipaddress=true -client-port=$CLIENT_PORT -dir=/opt/gfxd/server1 -locators=$LOCATOR_1_PORT_10334_TCP_ADDR:$LOCATOR_1_PORT_10334_TCP_PORT
